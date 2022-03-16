0d205d76bb88a4527fed16edeacf8213310e5e12
kviliev <kalin.viliev@gmail.com>
2022-03-16 13:20:11 +0000

    Update pipe1.groovy

--- pipelines/pipe1.groovy
+++ pipelines/pipe1.groovy
@@ -1,1 +1,29 @@
-test
+[
+     [region: 'UK'],
+
+].each { Map params ->
+   [ 
+       [environment: 'dev', branch: 'master', project: 'dev-app-project'],
+       [environment: 'qa', branch: 'master', project: 'qa-app-project'],  
+   ].each { Map paramsEnv ->
+       pipelineJob("${params.region}/${paramsEnv.environment}/${paramsEnv.project}/app-demo1") {
+    
+    def repo = 'https://github.com/jenkinsci/pipeline-examples'       
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                           url(repo)
+                           //github('jenkinsci/pipeline-examples')
+                        }
+                        branches("${paramsEnv.branch}")
+                        scriptPath('declarative-examples/simple-examples/environmentInStage.groovy')
+                        extensions {}
+                     }
+                  }
+                }
+            }
+        }
+    }
+}

9c31d899d138e1ed0e5c5a2d94c6e4e2c15fad1c
kviliev <kalin.viliev@gmail.com>
2022-03-24 16:33:29 +0000

    Update pipe.groovy

--- pipelines/pipe.groovy
+++ pipelines/pipe.groovy
@@ -10,7 +10,7 @@
             parameters {
             choice(
                      name: 'ENV',
-                     choices: ['dev', 'test']
+                     choices: "dev\ntest"
               )
             }
     
